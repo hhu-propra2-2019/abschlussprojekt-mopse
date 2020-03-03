c17463ecda062c874542bb11a3696da3ffd7beb6
KaFe23 <kai.feder.hhg@gmx.de>
2020-03-03 12:20:52 +0100

    FIX: gradle setup

--- src/test/java/mops/Material1ApplicationTests.java
+++ src/test/java/mops/Material1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Material1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
