589ad7636aceb1ae85427cadc21cf331b6d7da62
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 22:10:19 +0100

    ADD: getter for id

--- src/main/java/mops/persistence/Directory.java
+++ src/main/java/mops/persistence/Directory.java
@@ -4,4 +4,8 @@ package mops.persistence;
  * Represents a 'directory' of the file server.
  */
 public interface Directory {
+    /**
+     * @return the id of the folder.
+     */
+    Long getId();
 }
