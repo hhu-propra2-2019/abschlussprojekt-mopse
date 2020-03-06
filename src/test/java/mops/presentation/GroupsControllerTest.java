eef7a8a0358af9202692a6e5d1fbdf17a07bfa2f
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 16:56:44 +0100

    REFACTOR: mock annotation is redundant

--- src/test/java/mops/presentation/GroupsControllerTest.java
+++ src/test/java/mops/presentation/GroupsControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -71,7 +70,6 @@ public class GroupsControllerTest {
      * Test if all groups are presented in the index view.
      */
     @Test
-    @WithMockUser(username = "studi", roles = { "studentin" })
     public void getAllGroups() throws Exception {
         SecurityContextUtil.setupSecurityContextMock("userName", "userEmail@mail.de", Set.of("studentin"));
         mvc.perform(get("/material1/groups/"))
