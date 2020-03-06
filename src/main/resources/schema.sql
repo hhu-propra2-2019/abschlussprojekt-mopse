d386f581b9e3a4a12d981ec1957fdde5e424764d
Ali Hassoun <ali-hassoun.97@hotmail.de>
2020-03-06 10:17:15 +0100

    UPDATE: schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,37 +1,43 @@
 DROP TABLE IF EXISTS file_info;
 CREATE TABLE file_info
 (
-    id        integer PRIMARY KEY AUTO_INCREMENT,
-    name      text,
-    directory integer,
-    type      text,
-    size      integer,
-    owner     text
+    id        BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name      TEXT NOT NULL CHECK (name NOT LIKE ''),
+    directoryID BIGINT NOT NULL,
+    type      TEXT NOT NULL CHECK(type NOT LIKE ''),
+    size      BIGINT NOT NULL,
+    owner     TEXT NOT NULL CHECK(owner NOT LIKE ''),
+    FOREIGN KEY(directoryID) REFERENCES directory(id),
+    UNIQUE(name, directoryID)
 );
 
 DROP TABLE IF EXISTS directory;
 CREATE TABLE directory
 (
-    id                    integer PRIMARY KEY AUTO_INCREMENT,
-    name                  text,
-    parent                integer,
-    group_owner           integer
+    id                    BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                  TEXT NOT NULL,
+    parentID              BIGINT,
+    group_owner           INTEGER NOT NULL,
+    permission_id          BIGINT NOT NULL,
+    FOREIGN KEY(parentID) REFERENCES directory(id),
+    FOREIGN KEY(permissionID) REFERENCES directory_permissions(id) ,
+    UNIQUE(name, parentID, group_owner)
 );
 
 DROP TABLE IF EXISTS directory_permissions;
 CREATE TABLE directory_permissions
 (
-    id        integer PRIMARY KEY AUTO_INCREMENT,
-    directory integer
+    id        BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 );
 
 DROP TABLE IF EXISTS directory_permission_entry;
 CREATE TABLE directory_permission_entry
 (
-    id         integer PRIMARY KEY AUTO_INCREMENT,
-    permission integer,
-    role       text,
-    can_read   bool,
-    can_write  bool,
-    can_delete bool
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    permission_id BIGINT NOT NULL,
+    role       TEXT NOT NULL CHECK(text NOT LIKE ''),
+    can_read   BOOLEAN NOT NULL,
+    can_write  BOOLEAN NOT NULL,
+    can_delete BOOLEAN NOT NULL,
+    FOREIGN KEY(permissionID) REFERENCES directory_permissions(id);
 );
