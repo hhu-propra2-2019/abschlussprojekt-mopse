bf4ed4d76874e4801d96022830f2adaed96602e2
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 21:45:41 +0100

    ADD: get sub folders method

--- src/main/java/mops/businesslogic/DirectoryService.java
+++ src/main/java/mops/businesslogic/DirectoryService.java
@@ -1,6 +1,9 @@
 package mops.businesslogic;
 
+import mops.persistence.Directory;
 import mops.persistence.FileInfo;
+
+import java.util.List;
 
 public interface DirectoryService {
     /**
@@ -11,4 +14,13 @@ public interface DirectoryService {
      * @param fileInfo the file object
      */
     void uploadFile(Account account, int dirId, FileInfo fileInfo);
+
+    /**
+     * Returns all folders of the parent folder.
+     *
+     * @param account user credentials
+     * @param dirId   id of the folder
+     * @return list of folders
+     */
+    List<Directory> getSubFolders(Account account, int dirId);
 }
