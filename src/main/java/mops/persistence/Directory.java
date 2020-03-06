414c663a025b5b5de5d9d72fa0a9046b2461d851
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 22:24:11 +0100

    REMOVE: redundant method

--- src/main/java/mops/persistence/Directory.java
+++ src/main/java/mops/persistence/Directory.java
@@ -4,8 +4,4 @@ package mops.persistence;
  * Represents a 'directory' of the file server.
  */
 public interface Directory {
-    /**
-     * @return the id of the folder.
-     */
-    Long getId();
 }
