5c9c9b1cee042f3cdcf292bfaf46afcb72244a40
Marcel <marcel.haas@hhu.de>
2020-03-06 16:43:05 +0100

    Feature/group controller (#80)
    * ADD: file service interface
    
    * ADD:  file info interface
    
    * ADD: initial controller
    
    * FIX: FileService mock
    
    this will later be removed when implemented
    
    * ADD: Controller test for route `/material1/group`
    
    * ADD: directory template
    
    * ADD: controller for route `/material1/group`
    
    * ADD: file query interface
    
    * ADD: method for searching files in a group
    
    * UPDATE: add account as parameter
    
    * UPDATE: add account as parameter
    
    * ADD: search method
    
    * ADD: test for searching files in group
    
    * FIX: test send crsf
    
    * ADD: java doc
    
    * FIX: java doc
    
    * UPDATE: rename template to files
    
    * ADD: Controller to doc
    
    * UPDATE: PMD to allow longer comments
    
    * UPDATE: get query as parameter
    
    * FIX: typos
    
    * UPDATE: remove heading
    
    Co-Authored-By: iTitus <iTitus@users.noreply.github.com>
    
    Co-authored-by: iTitus <iTitus@users.noreply.github.com>

--- src/test/java/mops/presentation/GroupControllerTest.java
+++ src/test/java/mops/presentation/GroupControllerTest.java
@@ -1,9 +1,10 @@
 package mops.presentation;
 
 import mops.businesslogic.Account;
+import mops.businesslogic.FileQuery;
 import mops.businesslogic.FileService;
 import mops.businesslogic.GroupService;
-import mops.persistence.Directory;
+import mops.persistence.FileInfo;
 import mops.presentation.utils.SecurityContextUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,45 +12,42 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SuppressWarnings("PMD")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class GroupsControllerTest {
-
-
+public class GroupControllerTest {
     /**
      * Necessary mock until GroupService is implemented.
      */
     @MockBean
     private GroupService groupService;
     /**
-     * Necessary mock until FileService is implemented.
+     * Necessary mock until GroupService is implemented.
      */
     @MockBean
     private FileService fileService;
+
     /**
      * Necessary bean.
      */
     @Autowired
     private WebApplicationContext context;
-
-
     /**
      * Necessary bean.
      */
@@ -59,28 +57,42 @@ public class GroupsControllerTest {
      * Setups the a Mock MVC Builder.
      */
     @BeforeEach
-    public void setUp() {
-        List<Directory> groupList = new ArrayList<>();
+    void setUp() {
+        ArrayList<FileInfo> files = new ArrayList<>();
         final Account account = new Account("studi", "bla@bla.de", "pic.png", Set.of("studentin"));
-        given(groupService.getAllGroupRootDirectories(account)).willReturn(groupList);
+        given(fileService.getAllFilesOfGroup(account, 1)).willReturn(files);
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .alwaysDo(print())
                 .apply(springSecurity())
                 .build();
-
     }
 
     /**
-     * Test if all groups are presented in the index view.
+     * Tests if all files of the a group are returned.
      */
     @Test
-    @WithMockUser(username = "studi", roles = {"studentin"})
-    public void getAllGroups() throws Exception {
+    public void getAllFilesOfDirectory() throws Exception {
         SecurityContextUtil.setupSecurityContextMock("userName", "userEmail@mail.de", Set.of("studentin"));
-        mvc.perform(get("/material1/groups/"))
+        mvc.perform(get("/material1/group/1"))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(view().name("groups"));
+                .andExpect(view().name("files"));
+    }
+
+    /**
+     * Tests if the correct view is called upon searching in a group.
+     */
+    @Test
+    public void searchFile() throws Exception {
+        FileQuery fileQuery = mock(FileQuery.class);
+        SecurityContextUtil.setupSecurityContextMock("userName",
+                "userEmail@mail.de",
+                Set.of("studentin"));
+        mvc.perform(post("/material1/group/1/search")
+                .requestAttr("searchQuery", fileQuery)
+                .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("files"));
     }
 
     /**
@@ -90,7 +102,8 @@ public class GroupsControllerTest {
      */
     @Test
     public void notSignedIn() throws Exception {
-        mvc.perform(get("/material1/groups/"))
+        mvc.perform(get("/material1/group/1"))
                 .andExpect(status().is3xxRedirection());
     }
+
 }
