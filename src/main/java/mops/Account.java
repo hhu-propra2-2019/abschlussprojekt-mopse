05a959d22d83ad3bf3aa17ad5ff78ea898cb0781
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 10:43:51 +0100

    ADD: require args constructor

--- src/main/java/mops/Account.java
+++ src/main/java/mops/Account.java
@@ -2,26 +2,32 @@ package mops;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
 @Getter
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class Account {
     /**
      * Name of the user.
      */
+    @NonNull
     private final String name;
     /**
      * Email of the user.
      */
+    @NonNull
     private final String email;
     /**
      * Avatar of the user.
      */
-    private final String image;
+    private String image;
     /**
      * Keycloak roles of the user.
      */
+    @NonNull
     private final Set<String> roles;
 }
