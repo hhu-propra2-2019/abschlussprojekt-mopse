f016a531b2c8b7cea2fe06cc1314a62c4f7bd024
Segelzwerg <marcel.haas@hhu.de>
2020-03-06 21:49:37 +0100

    ADD: route for show content of folder

--- src/main/java/mops/presentation/DirectoryController.java
+++ src/main/java/mops/presentation/DirectoryController.java
@@ -3,7 +3,9 @@ package mops.presentation;
 import lombok.AllArgsConstructor;
 import mops.businesslogic.Account;
 import mops.businesslogic.DirectoryService;
+import mops.businesslogic.FileService;
 import mops.businesslogic.utils.AccountUtil;
+import mops.persistence.Directory;
 import mops.persistence.FileInfo;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/material1/dir")
 @AllArgsConstructor
@@ -22,6 +26,11 @@ public class DirectoryController {
      * Manages all directory queries.
      */
     private final DirectoryService directoryService;
+
+    /**
+     * Manges all file queries.
+     */
+    private final FileService fileService;
 
     /**
      * Uploads a file.
@@ -37,10 +46,28 @@ public class DirectoryController {
                              Model model,
                              @PathVariable("dirId") int dirId,
                              @Param("file") FileInfo fileInfo) {
-        Account account = AccountUtil.getAccountFromToken(token);
+        final Account account = AccountUtil.getAccountFromToken(token);
         //TODO: exception handling and user error message
         directoryService.uploadFile(account, dirId, fileInfo);
         return String.format("redirect:/material1/dir/%d", dirId);
+    }
+
+    /**
+     * @param token keycloak auth token
+     * @param model spring view model
+     * @param dirId id of the folder
+     * @return route to folder
+     */
+    @GetMapping(path = "/{dirId}")
+    public String showFolderContent(KeycloakAuthenticationToken token,
+                                    Model model,
+                                    @PathVariable("dirId") int dirId) {
+        final Account account = AccountUtil.getAccountFromToken(token);
+        final List<Directory> directories = directoryService.getSubFolders(account, dirId);
+        final List<FileInfo> files = fileService.getFilesOfDirectory(account, dirId);
+        model.addAttribute("dirs", directories);
+        model.addAttribute("files", files);
+        return String.format("directory%d", dirId);
     }
 }
 
