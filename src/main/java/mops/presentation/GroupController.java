cff8e1d6d117ee471c0a01343f87f1df2d7f804f
Marcel <marcel.haas@hhu.de>
2020-03-07 23:06:22 +0100

    Update src/main/java/mops/presentation/GroupController.java
    sorry missed at merge conflicts
    
    Co-Authored-By: iTitus <iTitus@users.noreply.github.com>

--- src/main/java/mops/presentation/GroupController.java
+++ src/main/java/mops/presentation/GroupController.java
@@ -51,7 +51,7 @@ public class GroupController {
     @PostMapping(path = "/{groupId}/search")
     public String searchFilesInGroup(KeycloakAuthenticationToken token,
                                      Model model,
-                                     @PathVariable("groupId") int groupId,
+                                     @PathVariable("groupId") long groupId,
                                      @ModelAttribute("searchQuery") FileQuery query) {
         final Account account = AccountUtil.getAccountFromToken(token);
         final List<FileInfo> files = fileService.searchFilesInGroup(account, groupId, query);
