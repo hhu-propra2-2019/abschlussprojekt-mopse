3c45ef55ffa0f835755cb99cd348c4bed840f141
kosch104 <Konsicrafter@web.de>
2020-03-09 11:31:57 +0100

    ADD: Javadoc to directory and FileInfo

--- src/main/java/mops/persistence/Directory.java
+++ src/main/java/mops/persistence/Directory.java
@@ -4,6 +4,12 @@ package mops.persistence;
  * Represents a 'directory' of the file server.
  */
 public interface Directory {
+    /**
+     * Unique directory id.
+     */
     int id = 0;
+    /**
+     * Display Name of a directory.
+     */
     String name = null;
 }
--- src/main/java/mops/persistence/FileInfo.java
+++ src/main/java/mops/persistence/FileInfo.java
@@ -4,8 +4,20 @@ package mops.persistence;
  * Represents a 'file' of the file server.
  */
 public interface FileInfo {
+   /**
+    * Display Name of a file.
+    */
    String name = null;
+   /**
+    * Creation date of a file.
+    */
    int date = 0;
+   /**
+    * Content Type of a File (pdf, png, etc.).
+    */
    String contentType = null;
+   /**
+    * Byte size of a file
+    */
    int size = 0;
 }
