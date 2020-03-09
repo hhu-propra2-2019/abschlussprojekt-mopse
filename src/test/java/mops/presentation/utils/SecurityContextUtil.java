8b99d904b39bae505757c7a166271c45c1ee1d9b
Segelzwerg <marcel.haas@hhu.de>
2020-03-09 10:43:17 +0100

    ADD: context build from account

--- src/test/java/mops/presentation/utils/SecurityContextUtil.java
+++ src/test/java/mops/presentation/utils/SecurityContextUtil.java
@@ -1,5 +1,6 @@
 package mops.presentation.utils;
 
+import mops.businesslogic.Account;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
 import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
@@ -12,6 +13,15 @@ import java.util.Set;
 import static org.mockito.Mockito.*;
 
 public class SecurityContextUtil {
+
+    /**
+     * Builds a security context using mock for keycloak.
+     *
+     * @param account the user
+     */
+    public static void setupSecurityContextMock(Account account) {
+        setupSecurityContextMock(account.getName(), account.getEmail(), account.getRoles());
+    }
 
     /**
      * Builds a security context using mock for keycloak.
@@ -31,7 +41,7 @@ public class SecurityContextUtil {
      * @param userEmail email of the user
      * @param roles     roles of the user
      */
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({"PMD", "rawtypes"})
     public static void setupSecurityContextMock(String userName, String userEmail, Set<String> roles) {
         KeycloakPrincipal principal = mock(KeycloakPrincipal.class, RETURNS_DEEP_STUBS);
         when(principal.getName()).thenReturn(userName);
