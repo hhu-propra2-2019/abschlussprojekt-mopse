7e0ca1ba3d1a9979f88ba247b68987c81c7b8422
Segelzwerg <marcel.haas@hhu.de>
2020-03-09 12:16:16 +0100

    UPDATE: use lombok for constructor

--- src/main/java/mops/businesslogic/GroupURLWrapper.java
+++ src/main/java/mops/businesslogic/GroupURLWrapper.java
@@ -2,25 +2,18 @@ package mops.businesslogic;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 
 /**
  * Ignore unused private field warning
  */
-@SuppressWarnings("PMD")
+@AllArgsConstructor
 public class GroupURLWrapper {
     /**
      * The id of the group.
      */
     @JsonProperty("group_id")
     private final long groupId;
-
-    /**
-     * @param groupId the of the group
-     */
-    public GroupURLWrapper(long groupId) {
-        this.groupId = groupId;
-
-    }
 
     /**
      * The relative url of the group root dir.
