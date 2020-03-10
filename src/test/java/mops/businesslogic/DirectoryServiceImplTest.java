7c7150751a9a0bdd1f48337e0924393c34c29295
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 10:19:17 +0100

    ADD: account in setup

--- src/test/java/mops/businesslogic/DirectoryServiceImplTest.java
+++ src/test/java/mops/businesslogic/DirectoryServiceImplTest.java
@@ -1,9 +1,12 @@
 package mops.businesslogic;
 
 import mops.SpringTestContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Set;
 
 @SpringTestContext
 @SpringBootTest
@@ -25,4 +28,17 @@ public class DirectoryServiceImplTest {
      */
     @Autowired
     private DirectoryService directoryService;
+
+    /**
+     * Account Object containing user credentials.
+     */
+    private Account account;
+
+    /**
+     * Creates a user account.
+     */
+    @BeforeEach
+    void setUp() {
+        account = new Account("user", "user@hhu.de", Set.of("studentin"));
+    }
 }
