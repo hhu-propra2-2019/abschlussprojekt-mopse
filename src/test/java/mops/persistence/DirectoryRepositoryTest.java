775319f0b96f5138369293018f522fc515d79567
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 14:57:16 +0100

    ADD: test for sub folder fetch

---
+++ src/test/java/mops/persistence/DirectoryRepositoryTest.java
@@ -0,0 +1,46 @@
+package mops.persistence;
+
+import mops.persistence.directory.Directory;
+import mops.persistence.permission.DirectoryPermissions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+public class DirectoryRepositoryTest {
+
+    /**
+     * Handles db communication for directories.
+     */
+    @Autowired
+    private DirectoryRepository directoryRepository;
+    /**
+     * Handles db communication for directory permissions.
+     */
+    @Autowired
+    private DirectoryPermissionsRepository directoryPermissionsRepository;
+
+    /**
+     * Tests if sub folders are correctly returned.
+     */
+    @Test
+    public void getAllSubFoldersOfParent() {
+        long groupOwner = 1L;
+        long permissionsId = directoryPermissionsRepository.save(new DirectoryPermissions()).getId();
+        Directory root = new Directory("root", null, groupOwner, permissionsId);
+        Directory savedRoot = directoryRepository.save(root);
+
+        Directory first = new Directory("first", savedRoot.getId(), groupOwner, permissionsId);
+        Directory second = new Directory("second", savedRoot.getId(), groupOwner, permissionsId);
+
+        List<Directory> savedDirectories = (List<Directory>) directoryRepository.saveAll(List.of(first, second));
+
+        List<Directory> allSubFoldersOfParent = directoryRepository.getAllSubFoldersOfParent(savedRoot.getId());
+
+        assertThat(allSubFoldersOfParent).isEqualTo(savedDirectories);
+    }
+}
