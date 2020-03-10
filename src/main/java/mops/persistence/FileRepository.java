0a65bcab07eb93b4572101e980c1a7d4fcf51c31
realemx <49307158+realemx@users.noreply.github.com>
2020-03-10 10:05:53 +0100

    ADD: FileRepository

---
+++ src/main/java/mops/persistence/FileRepository.java
@@ -0,0 +1,117 @@
+package mops.persistence;
+
+import io.minio.MinioClient;
+import io.minio.ServerSideEncryption;
+import io.minio.errors.*;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
+import java.util.HashMap;
+
+
+@Repository
+public class FileRepository {
+
+    /**
+     * The MinIO client.
+     */
+    private MinioClient minioClient;
+
+    /**
+     * Injected MinIO configuration.
+     */
+//    @Autowired
+    private FileRepositoryConfiguration configuration;
+
+    /**
+     * Connects to MinIO Server and checks if the bucket exists.
+     * @param configuration the injected Conf.
+     */
+    public FileRepository(FileRepositoryConfiguration configuration) {
+        this.configuration = configuration;
+        try {
+            this.minioClient = new MinioClient(
+                    configuration.getHost(),
+                    configuration.getPort(),
+                    configuration.getAccessKey(),
+                    configuration.getSecretKey()
+            );
+
+            if (!minioClient.bucketExists(configuration.getBucketName())) {
+                minioClient.makeBucket(configuration.getBucketName());
+            }
+
+        } catch (InvalidEndpointException e) {
+            System.err.println("MinIO endpoint not found.");
+            System.exit(1);
+        } catch (InvalidPortException e) {
+            System.err.println("MinIO port invalid.");
+            System.exit(1);
+        } catch (Exception e) {
+            // Multiple, unlikely exceptions ¯\_(ツ)_/¯
+            System.err.println(e.getClass().toString() + ": " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Saves a file.
+     * @param file the file that should get saved permanently.
+     * @param fileId the File ID given by the FileInfo database.
+     * @return true if successful; false if not.
+     */
+    public boolean saveFile(MultipartFile file, Long fileId) {
+
+        try {
+            minioClient.putObject(configuration.getBucketName(),
+                    fileId.toString(),
+                    file.getInputStream(),
+                    file.getSize(),
+                    new HashMap<String, String>(),
+                    null, // no encryption will be needed
+                    file.getContentType()
+            );
+        } catch (Exception e) {
+            System.err.println("WTF!?: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Deletes a file permanently.
+     * @param fileId the ID of the file that's desired to be deleted.
+     * @return true if successful; false if not.
+     */
+    public boolean deleteFile(Long fileId) {
+        try {
+            minioClient.removeObject(
+                    configuration.getBucketName(),
+                    fileId.toString()
+            );
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+    /**
+     * Retrieves the bytes of the file.
+     * @param fileId the ID of the file that's desired to be returned.
+     * @return file content as InputStream.
+     */
+    public InputStream getFileContent(Long fileId) {
+        InputStream bytes = null;
+        try {
+            bytes =  minioClient.getObject(
+                    configuration.getBucketName(),
+                    fileId.toString()
+            );
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+        return bytes;
+    }
+}
