0308f2bdd0b957b916e157822e57da16432a3c79
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 10:11:31 +0100

    ADD: mock beans

--- src/test/java/mops/businesslogic/DirectoryServiceImplTest.java
+++ src/test/java/mops/businesslogic/DirectoryServiceImplTest.java
@@ -1,5 +1,22 @@
 package mops.businesslogic;
 
+import mops.SpringTestContext;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringTestContext
+@SpringBootTest
 public class DirectoryServiceImplTest {
+    /**
+     * Necessary bean, must be removed when file service is implemented.
+     */
+    @MockBean
+    private FileService fileService;
+
+    /**
+     * Necessary bean, must be removed when group service is implemented.
+     */
+    @MockBean
+    private GroupService groupService;
 
 }
