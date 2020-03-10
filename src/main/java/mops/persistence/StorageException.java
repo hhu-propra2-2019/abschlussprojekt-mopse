51a72dec344bc878a80d6d75f77303a3b91a43c4
realemx <49307158+realemx@users.noreply.github.com>
2020-03-10 17:21:53 +0100

    ADD: custom exception

---
+++ src/main/java/mops/persistence/StorageException.java
@@ -0,0 +1,12 @@
+package mops.persistence;
+
+@SuppressWarnings("PMD.MissingSerialVersionUID")
+public class StorageException extends Exception  {
+    /**
+     * Custom exception for everything while handling with the File Storage.
+     * @param exc original exception
+     */
+    public StorageException(Exception exc) {
+        super(exc);
+    }
+}
