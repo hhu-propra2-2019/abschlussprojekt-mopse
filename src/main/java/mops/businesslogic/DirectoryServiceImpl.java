f509074cc8471b6fa5d2f6446b755bcc380dc298
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 10:01:31 +0100

    ADD: Directory Service Implementation

---
+++ src/main/java/mops/businesslogic/DirectoryServiceImpl.java
@@ -0,0 +1,70 @@
+package mops.businesslogic;
+
+import mops.persistence.directory.Directory;
+import mops.persistence.file.FileInfo;
+
+import java.util.List;
+
+public class DirectoryServiceImpl implements DirectoryService {
+    /**
+     * Uploads a file.
+     *
+     * @param account  user credentials
+     * @param dirId    the id of the folder where the file will be uploaded
+     * @param fileInfo the file object
+     */
+    @Override
+    public void uploadFile(Account account, long dirId, FileInfo fileInfo) {
+
+    }
+
+    /**
+     * Returns all folders of the parent folder.
+     *
+     * @param account user credentials
+     * @param dirId   id of the folder
+     * @return list of folders
+     */
+    @Override
+    public List<Directory> getSubFolders(Account account, long dirId) {
+        return null;
+    }
+
+    /**
+     * Creates a new folder inside a folder.
+     *
+     * @param account     user credentials
+     * @param parentDirId id of the parent folder
+     * @param dirName     name of the new folder
+     * @return id of the new folder
+     */
+    @Override
+    public long createFolder(Account account, long parentDirId, String dirName) {
+        return 0;
+    }
+
+    /**
+     * Deletes a folder.
+     *
+     * @param account user credential
+     * @param dirId   id of the folder to be deleted
+     * @return the parent id of the deleted folder
+     */
+    @Override
+    public long deleteFolder(Account account, long dirId) {
+        return 0;
+    }
+
+    /**
+     * Searches a folder for files.
+     *
+     * @param account user credentials
+     * @param dirId   id of the folder to be searched
+     * @param query   wrapper object of the query parameter
+     * @return list of files
+     */
+    @Override
+    public List<FileInfo> searchFolder(Account account, long dirId, FileQuery query) {
+        return null;
+    }
+}
