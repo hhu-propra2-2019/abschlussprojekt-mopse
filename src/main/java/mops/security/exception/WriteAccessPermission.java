3d6ed1c321fcc3a94c16c0c0e03079f3bafb5807
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 16:52:37 +0100

    ADD: IDE generated serialVersionUID

--- src/main/java/mops/security/exception/WriteAccessPermission.java
+++ src/main/java/mops/security/exception/WriteAccessPermission.java
@@ -1,6 +1,8 @@
 package mops.security.exception;
 
 public class WriteAccessPermission extends Exception {
+    private static final long serialVersionUID = 588335701190058578L;
+
     /**
      * Is thrown when a user tries to write to a directory for which he/she/it does not have writing permissions.
      *
