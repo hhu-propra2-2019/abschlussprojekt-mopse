b412bf924e68df893f2d853d96e170b6dd2619c9
Segelzwerg <marcel.haas@hhu.de>
2020-03-10 11:03:49 +0100

    ADD: query for getting sub folders

--- src/main/java/mops/persistence/DirectoryRepository.java
+++ src/main/java/mops/persistence/DirectoryRepository.java
@@ -1,9 +1,19 @@
 package mops.persistence;
 
 import mops.persistence.directory.Directory;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DirectoryRepository extends CrudRepository<Directory, Long> {
+    /**
+     * @param parentId the id of the parent folder
+     * @return a list of level one sub folder of the parent folder
+     */
+    @Query("SELECT * FROM directory WHERE parentId IS NOT NULL AND parentId = :parentId")
+    List<Directory> getAllSubFoldersOfParent(@Param("parentId") long parentId);
 }
