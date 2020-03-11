43ed195d1ab2bcb6db885184f7eb1920d5db243e
iTitus <iTitus@users.noreply.github.com>
2020-03-11 19:16:30 +0100

    CHANGE: let spring create the MockMvc

--- src/test/java/mops/SecurityTests.java
+++ src/test/java/mops/SecurityTests.java
@@ -7,23 +7,19 @@ import mops.businesslogic.FileService;
 import mops.businesslogic.GroupService;
 import mops.persistence.FileRepository;
 import mops.utils.TestContext;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestContext
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecurityTests {
 
     @MockBean
@@ -36,20 +32,7 @@ class SecurityTests {
     DirectoryService directoryService;
 
     @Autowired
-    WebApplicationContext context;
-
     MockMvc mvc;
-
-    /**
-     * Necessary configuration for Spring Security tests.
-     */
-    @BeforeAll
-    void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
 
     /**
      * Tests auth needed on index.
