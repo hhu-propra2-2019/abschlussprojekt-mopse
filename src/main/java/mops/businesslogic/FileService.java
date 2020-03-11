d3dfc9fc32bb7a9a3ddff9fecae40c8bacdfd0ca
kosch104 <Konsicrafter@web.de>
2020-03-11 11:10:28 +0100

    ADD: MopsExceptions to FileService

--- src/main/java/mops/businesslogic/FileService.java
+++ src/main/java/mops/businesslogic/FileService.java
@@ -15,7 +15,7 @@ public interface FileService {
      * @param groupId group identification
      * @return list of all files in that directory
      */
-    List<FileInfo> getAllFilesOfGroup(Account account, long groupId);
+    List<FileInfo> getAllFilesOfGroup(Account account, long groupId) throws MopsException;
 
     /**
      * Searches for files in a group.
@@ -25,14 +25,14 @@ public interface FileService {
      * @param query   a query which specifies the serach
      * @return a list of files
      */
-    List<FileInfo> searchFilesInGroup(Account account, long groupId, FileQuery query);
+    List<FileInfo> searchFilesInGroup(Account account, long groupId, FileQuery query) throws MopsException;
 
     /**
      * @param account user credentials
      * @param dirId   id of the folder
      * @return a list of file in that folder
      */
-    List<FileInfo> getFilesOfDirectory(Account account, long dirId);
+    List<FileInfo> getFilesOfDirectory(Account account, long dirId) throws MopsException;
 
     /**
      * @param account user credentials
@@ -46,5 +46,5 @@ public interface FileService {
      * @param fileId  file id of file to be deleted
      * @return parent directory Id
      */
-    long deleteFile(Account account, long fileId);
+    long deleteFile(Account account, long fileId) throws MopsException;
 }
