b0da0463916955a6e502556e703ac7333b15ddbc
Segelzwerg <marcel.haas@hhu.de>
2020-03-11 11:08:21 +0100

    ADD: file service interface

--- src/main/java/mops/businesslogic/FileService.java
+++ src/main/java/mops/businesslogic/FileService.java
@@ -1,4 +1,21 @@
 package mops.businesslogic;
 
+import mops.persistence.file.FileInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
 public interface FileService {
+    /**
+     * @param account user credentials
+     * @param dirId   directory id
+     * @return a list of files
+     */
+    //TODO: change return to list of FileContainer
+    List<FileInfo> getFilesOfDirectory(Account account, long dirId);
+
+    void uploadFile(Account account, long dirId, MultipartFile multipartFile, Set<Object> of);
 }
