dbb3952186d739039a1d146f8cf043b9c395c24f
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-12 10:16:09 +0100

    ADD: Full FileInfoService Interface

--- src/main/java/mops/businesslogic/FileInfoService.java
+++ src/main/java/mops/businesslogic/FileInfoService.java
@@ -1,5 +1,6 @@
 package mops.businesslogic;
 
+import mops.exception.MopsException;
 import mops.persistence.file.FileInfo;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +16,31 @@ public interface FileInfoService {
      * @return a list of files in that directory
      */
     List<FileInfo> fetchAllFilesInDirectory(long dirId);
+
+    /**
+     *
+     * @param fileId file id
+     * @return a FileInfo object
+     */
+    FileInfo fetchFileInfo(long fileId);
+
+    /**
+     *
+     * @param fileInfo Metadata of a file
+     * @return ID the FileInfo was saved under
+     */
+    long saveFileInfo (FileInfo fileInfo);
+
+    /**
+     *
+     * @param fileId file id
+     */
+    void deleteFileInfo (long fileId) throws MopsException;
+
+    /**
+     *
+     * @param fileId file id
+     * @return dir id
+     */
+    long fetchDirectoryId (long fileId);
 }
