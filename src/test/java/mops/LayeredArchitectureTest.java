c40d3e7243477019abd99b5e749d4bb8b9e133e0
KaFe23 <kai.feder.hhg@gmx.de>
2020-03-12 08:11:14 +0100

    REFACTOR: Use importOptions in @AnalyzeClasses for cleaner code.

--- src/test/java/mops/LayeredArchitectureTest.java
+++ src/test/java/mops/LayeredArchitectureTest.java
@@ -1,18 +1,15 @@
 package mops;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTag;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import mops.businesslogic.DirectoryService;
-import mops.businesslogic.FileService;
-import mops.businesslogic.GroupService;
-import mops.persistence.FileRepository;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @ArchTag("checkLayeredArchitecture")
-@AnalyzeClasses(packages = "mops")
+@AnalyzeClasses(importOptions = ImportOption.DoNotIncludeTests.class, packages = "mops")
 public class LayeredArchitectureTest {
 
     /**
@@ -27,18 +24,5 @@ public class LayeredArchitectureTest {
 
             .whereLayer("mopsPresentation").mayNotBeAccessedByAnyLayer()
             .whereLayer("mopsBusinesslogic").mayOnlyBeAccessedByLayers("mopsPresentation")
-            .whereLayer("mopsPersistence").mayOnlyBeAccessedByLayers("mopsPresentation", "mopsBusinesslogic")
-
-            /*
-             * These dependencies are between the Tests and the different layers,
-             * which of course is correct and wanted and should not lead to a failing test.
-             */
-            .ignoreDependency(Material1ApplicationTests.class, DirectoryService.class)
-            .ignoreDependency(Material1ApplicationTests.class, FileRepository.class)
-            .ignoreDependency(Material1ApplicationTests.class, FileService.class)
-            .ignoreDependency(Material1ApplicationTests.class, GroupService.class)
-            .ignoreDependency(SecurityTests.class, DirectoryService.class)
-            .ignoreDependency(SecurityTests.class, FileRepository.class)
-            .ignoreDependency(SecurityTests.class, FileService.class)
-            .ignoreDependency(SecurityTests.class, GroupService.class);
+            .whereLayer("mopsPersistence").mayOnlyBeAccessedByLayers("mopsPresentation", "mopsBusinesslogic");
 }
