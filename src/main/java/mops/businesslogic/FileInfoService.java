636d7b5946bd8eff3080a845aceb16034457ebd4
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-12 09:30:52 +0100

    ADD: Initiation of FileInfoService

---
+++ src/main/java/mops/businesslogic/FileInfoService.java
@@ -0,0 +1,18 @@
+package mops.businesslogic;
+
+import mops.persistence.file.FileInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Handles meta data for files.
+ */
+@Service
+public interface FileInfoService {
+    /**
+     * @param dirId directory id
+     * @return a list of files in that directory
+     */
+    List<FileInfo> fetchAllFilesInDirectory(long dirId);
+}
