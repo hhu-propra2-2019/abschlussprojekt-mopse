81521d0d5f060772a81948e32e25bd5dc2b2972b
KaFe23 <kai.feder.hhg@gmx.de>
2020-03-12 11:35:23 +0100

    ADD: javadoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,4 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"mops"},{"l":"mops.businesslogic"},{"l":"mops.businesslogic.utils"},{"l":"mops.exception"},{"l":"mops.persistence"},{"l":"mops.persistence.directory"},{"l":"mops.persistence.file"},{"l":"mops.persistence.permission"},{"l":"mops.presentation"},{"l":"mops.security"},{"l":"mops.utils"}]
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "mops"}, {"l": "mops.businesslogic"}, {"l": "mops.businesslogic.utils"}, {"l": "mops.config"}, {"l": "mops.exception"}, {"l": "mops.persistence"}, {"l": "mops.persistence.directory"}, {"l": "mops.persistence.file"}, {"l": "mops.persistence.permission"}, {"l": "mops.presentation"}, {"l": "mops.utils"}];
