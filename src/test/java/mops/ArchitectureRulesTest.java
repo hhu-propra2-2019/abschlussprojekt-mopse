4c1dc474aefa0697a0ca2c9a624ba3e5ce92ffde
KaFe23 <kai.feder.hhg@gmx.de>
2020-03-12 09:59:46 +0100

    REFACTOR: Change @Test to @ArchTest

--- src/test/java/mops/ArchitectureRulesTest.java
+++ src/test/java/mops/ArchitectureRulesTest.java
@@ -7,7 +7,6 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import mops.utils.AggregateBuilder;
 import mops.utils.AggregateRoot;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.stereotype.Controller;
 
@@ -25,7 +24,7 @@ public class ArchitectureRulesTest {
      * having only one @AggregateRoot per class/package.
      */
     @ArchTest
-    static final ArchRule aggregateRootPublicNothingElse = classes()
+    static final ArchRule AGGREGATE_ROOT_PUBLIC_NOTHING_ELSE = classes()
             .that()
             .areNotAnnotatedWith(AggregateRoot.class)
             .and()
@@ -40,31 +39,23 @@ public class ArchitectureRulesTest {
      * Tests if there is only one Aggregate Root per package
      * for outer communication.
      */
-    @Test
-    public void oneAggregateRootPerAggregate() {
-        ArchRule oneAggregateRootPerPackage = slices()
-                .matching(".." + ArchitectureRuleConfig.MOPS_PERSISTENCE)
-                .should(HAVE_EXACTLY_ONE_AGGREGATE_ROOT);
-
-        oneAggregateRootPerPackage.check(ArchitectureRuleConfig.JAVA_CLASSES);
-    }
+    @ArchTest
+    static final ArchRule ONE_AGGREGATE_ROOT_PER_PACKAGE = slices()
+            .matching(".." + ArchitectureRuleConfig.MOPS_PERSISTENCE)
+            .should(HAVE_EXACTLY_ONE_AGGREGATE_ROOT);
 
     /**
      * This tests if there are any Controllers, that aren't in the Presentation layer,
      * which would be a violation of the Layer Architecture.
      */
-    @Test
-    public void allControllersShouldResideInMopsPresentation() {
-        ArchRule allControllersShouldResideInMopsPresentation = classes()
-                .that()
-                .areAnnotatedWith(Controller.class)
-                .and()
-                .resideOutsideOfPackage(ArchitectureRuleConfig.MOPS_PRESENTATION_BASE)
-                .should()
-                .notBeAnnotatedWith(Controller.class);
-
-        allControllersShouldResideInMopsPresentation.check(ArchitectureRuleConfig.JAVA_CLASSES);
-    }
+    @ArchTest
+    static final ArchRule ALL_CONTROLLERS_SHOULD_RESIDE_IN_MOPS_PRESENTATION = classes()
+            .that()
+            .areAnnotatedWith(Controller.class)
+            .and()
+            .resideOutsideOfPackage(ArchitectureRuleConfig.MOPS_PRESENTATION_BASE)
+            .should()
+            .notBeAnnotatedWith(Controller.class);
 
     /**
      * This checks, if everything in "presentation" is annotated with @Controller,
@@ -72,29 +63,22 @@ public class ArchitectureRulesTest {
      * the "areNotAnnotatedWith(SpringBootTest.class)" is there, so that test.mops.presentation,
      * which are the tests, will not be tested and cause a wrong outcome of the test.
      */
-    @Test
-    public void everythingInPresentationShouldBeAController() {
-        ArchRule everythingInPresentationShouldBeAController = classes()
-                .that()
-                .resideInAPackage(ArchitectureRuleConfig.MOPS_PRESENTATION_BASE)
-                .and()
-                .areNotAnnotatedWith(WebMvcTest.class)
-                .should()
-                .beAnnotatedWith(Controller.class);
-
-        everythingInPresentationShouldBeAController.check(ArchitectureRuleConfig.JAVA_CLASSES);
-    }
+    @ArchTest
+    static final ArchRule EVERYTHING_IN_PRESENTATION_SHOULD_BE_A_CONTROLLER = classes()
+            .that()
+            .resideInAPackage(ArchitectureRuleConfig.MOPS_PRESENTATION_BASE)
+            .and()
+            .areNotAnnotatedWith(WebMvcTest.class)
+            .should()
+            .beAnnotatedWith(Controller.class);
 
     /**
      * This tests, if there are no Cycles within the Packages.
      */
-    @Test
-    public void areThereAnyCyclesWithinPackages() {
-        ArchRule areThereAnyCyclesWithinPackages = slices()
-                .matching("mops.(*)..")
-                .should()
-                .beFreeOfCycles();
+    @ArchTest
+    static final ArchRule ARE_THERE_ANY_CYCLES_WITHIN_PACKAGES = slices()
+            .matching("mops.(*)..")
+            .should()
+            .beFreeOfCycles();
 
-        areThereAnyCyclesWithinPackages.check(ArchitectureRuleConfig.JAVA_CLASSES);
-    }
 }
