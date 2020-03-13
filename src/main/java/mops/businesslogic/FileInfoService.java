f549b789052fe8c1ce26cdae6873aadbaee68cdf
iTitus <iTitus@users.noreply.github.com>
2020-03-13 17:32:28 +0100

    FIX: test and service implementation of FileInfoService (#144)
    * FIX: test and service implementation
    
    * FIX: accidental run config deletion

--- src/main/java/mops/businesslogic/FileInfoService.java
+++ src/main/java/mops/businesslogic/FileInfoService.java
@@ -13,25 +13,34 @@ import java.util.List;
 public interface FileInfoService {
 
     /**
+     * Lists all files in a specific directory.
+     *
      * @param dirId directory id
      * @return a list of files in that directory
      */
     List<FileInfo> fetchAllFilesInDirectory(long dirId) throws MopsException;
 
     /**
+     * Get a file by id.
+     *
      * @param fileId file id
      * @return a FileInfo object
      */
     FileInfo fetchFileInfo(long fileId) throws MopsException;
 
     /**
+     * Save file to database.
+     *
      * @param fileInfo Metadata of a file
      * @return the freshly saved FileInfo
      */
     FileInfo saveFileInfo(FileInfo fileInfo) throws MopsException;
 
     /**
+     * Delete file from database by id.
+     *
      * @param fileId file id
      */
     void deleteFileInfo(long fileId) throws MopsException;
+
 }
