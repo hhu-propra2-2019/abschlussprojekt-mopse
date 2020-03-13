8bba1a981fa3b4a6452a8152741ee60e0f62900f
Segelzwerg <marcel.haas@hhu.de>
2020-03-13 10:42:39 +0100

    REFACTOR: file info to setup

--- src/test/java/mops/businesslogic/FileQueryImplTest.java
+++ src/test/java/mops/businesslogic/FileQueryImplTest.java
@@ -1,6 +1,7 @@
 package mops.businesslogic;
 
 import mops.persistence.file.FileInfo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,18 +9,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileQueryImplTest {
-    @Test
-    public void findOwnerTest() {
-        FileQuery fileQuery = FileQuery.builder()
-                .owners(List.of("iTitus"))
-                .build();
-        FileInfo fileInfo = FileInfo.builder()
+
+    private FileInfo fileInfo;
+
+    @BeforeEach
+    void setUp() {
+        fileInfo = FileInfo.builder()
                 .id(1L)
                 .name("cv")
                 .directory(1L)
                 .type("pdf")
                 .size(80L)
                 .owner("iTitus")
+                .build();
+    }
+
+    @Test
+    public void findOwnerTest() {
+        FileQuery fileQuery = FileQuery.builder()
+                .owners(List.of("iTitus"))
                 .build();
 
         assertThat(fileQuery.checkMatch(fileInfo)).isTrue();
