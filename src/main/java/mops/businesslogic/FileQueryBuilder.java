e100c3d914c0c0d518063e149950450ec0955f28
Segelzwerg <marcel.haas@hhu.de>
2020-03-13 11:02:54 +0100

    UPDATE: builder to file names FIX: fields must not be null but emtpy list

--- src/main/java/mops/businesslogic/FileQueryBuilder.java
+++ src/main/java/mops/businesslogic/FileQueryBuilder.java
@@ -1,9 +1,39 @@
 package mops.businesslogic;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class FileQueryBuilder {
+    /**
+     * list of owner to search for
+     */
     private List<String> owners;
+
+    /**
+     * names of files to search for
+     */
+    private List<String> fileNames;
+
+    /**
+     * Builds the object from it's information.
+     *
+     * @return file query object
+     */
+    public FileQuery build() {
+        if (fileNames == null) {
+            fileNames = List.of();
+        }
+        if (owners == null) {
+            owners = List.of();
+        }
+        return new FileQueryImpl(
+                fileNames,
+                owners
+        );
+    }
 
     /**
      * @param owners list of owner to search for
@@ -14,14 +44,13 @@ public class FileQueryBuilder {
         return this;
     }
 
+
     /**
-     * Builds the object from it's information.
-     *
-     * @return file query object
+     * @param fileNames names of files to search for
+     * @return this
      */
-    public FileQuery build() {
-        return new FileQueryImpl(
-                owners
-        );
+    public FileQueryBuilder names(List<String> fileNames) {
+        this.fileNames = fileNames;
+        return this;
     }
 }
