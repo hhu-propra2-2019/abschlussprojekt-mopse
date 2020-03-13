fac601724bba247fab1e09e016c65cce6315e573
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-13 09:52:11 +0100

    ADD: bullshit
    im frustrated

--- src/test/java/mops/businesslogic/FileInfoServiceTest.java
+++ src/test/java/mops/businesslogic/FileInfoServiceTest.java
@@ -1,10 +1,23 @@
 package mops.businesslogic;
 
+import mops.exception.MopsException;
 import mops.persistence.FileInfoRepository;
 import mops.persistence.FileRepository;
+import mops.persistence.file.FileInfo;
 import mops.utils.TestContext;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @TestContext
 @SpringBootTest
@@ -17,14 +30,31 @@ public class FileInfoServiceTest {
     @MockBean
     FileRepository fileRepository;
     @MockBean
-    FileInfoService fileInfoService;
-    @MockBean
     FileService fileService;
     @MockBean
     PermissionService permissionService;
-    
-    void fetchAllFilesInDirectory() {
 
+    @Autowired
+    FileInfoService fileInfoService;
+
+    List<FileInfo> fileInfoList;
+    FileInfo fileInfo1;
+
+    @BeforeEach
+    void setUp() {
+        fileInfoList = new ArrayList<FileInfo>();
+        fileInfo1 = mock(FileInfo.class);
+        fileInfoList.add(fileInfo1);
+
+        given(fileInfoRepository
+                .getAllFileInfoByDirectory(1L))
+                .willReturn(fileInfoList);
+    }
+
+    @Test
+    public void fetchAllFilesInDirectory() throws MopsException {
+        List<FileInfo> result = fileInfoService.fetchAllFilesInDirectory(1L);
+        assertThat(result).isEqualTo(fileInfoList);
     }
 
     void fetchFileInfo() {
