89355abed003d99f74059d0080d8743421c65f9a
Pascal Liedtke <49307158+realemx@users.noreply.github.com>
2020-03-16 10:04:50 +0100

    FIX: interfaces (#157)
    * FIX: interfaces
    
    * FIX: return type
    
    * FIX: Tests
    
    Co-authored-by: Ali Hassoun <32816259+ali3654@users.noreply.github.com>

--- src/main/java/mops/businesslogic/FileService.java
+++ src/main/java/mops/businesslogic/FileService.java
@@ -1,24 +1,17 @@
 package mops.businesslogic;
 
 import mops.exception.MopsException;
+import mops.persistence.directory.Directory;
 import mops.persistence.file.FileInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.Set;
+import java.util.List;
+
 
 @Service
 public interface FileService {
-
-    /**
-     * Returns all files of a group.
-     *
-     * @param account user credentials
-     * @param groupId group identification
-     * @return list of all files in that directory
-     */
-    List<FileInfo> getAllFilesOfGroup(Account account, long groupId) throws MopsException;
 
     /**
      * @param account       user credentials
@@ -26,24 +19,7 @@ public interface FileService {
      * @param multipartFile the binary code of the file
      * @param tags          the file tag
      */
-    void uploadFile(Account account, long dirId, MultipartFile multipartFile, Set<String> tags) throws MopsException;
-
-    /**
-     * Searches for files in a group.
-     *
-     * @param account user credentials
-     * @param groupId group identification for the group to be searched
-     * @param query   a query which specifies the serach
-     * @return a list of files
-     */
-    List<FileInfo> searchFilesInGroup(Account account, long groupId, FileQuery query) throws MopsException;
-
-    /**
-     * @param account user credentials
-     * @param dirId   id of the folder
-     * @return a list of file in that folder
-     */
-    List<FileInfo> getFilesOfDirectory(Account account, long dirId) throws MopsException;
+    void saveFile(Account account, long dirId, MultipartFile multipartFile, Set<String> tags) throws MopsException;
 
     /**
      * @param account user credentials
@@ -54,16 +30,30 @@ public interface FileService {
 
     /**
      * @param account user credentials
-     * @param fileId  file id of needed file
-     * @return file info
+     * @param fileId  file id of file to be deleted
+     * @return parent directory Id
+     */
+    Directory deleteFile(Account account, long fileId) throws MopsException;
+
+
+    /**
+     * Retrieves file meta data.
+     *
+     * @param account the account object.
+     * @param fileId the file ID
+     * @return the meta info.
+     * @throws MopsException on error.
      */
     FileInfo getFileInfo(Account account, long fileId) throws MopsException;
 
     /**
-     * @param account user credentials
-     * @param fileId  file id of file to be deleted
-     * @return parent directory Id
+     * Retrieves file meta data of all files in a directory.
+     *
+     * @param account the account object.
+     * @param dirId the file ID
+     * @return the meta info.
+     * @throws MopsException on error.
      */
-    long deleteFile(Account account, long fileId) throws MopsException;
+    List<FileInfo> getFilesOfDirectory(Account account, long dirId) throws MopsException;
 
 }
