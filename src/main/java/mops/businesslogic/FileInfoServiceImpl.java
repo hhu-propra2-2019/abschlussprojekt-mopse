01b846b26174a29b3b0af101178e1cfa630058d5
Segelzwerg <marcel.haas@hhu.de>
2020-03-16 11:17:01 +0100

    ADD: log for file info service

--- src/main/java/mops/businesslogic/FileInfoServiceImpl.java
+++ src/main/java/mops/businesslogic/FileInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package mops.businesslogic;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mops.businesslogic.exception.DatabaseException;
 import mops.exception.MopsException;
 import mops.persistence.FileInfoRepository;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class FileInfoServiceImpl implements FileInfoService {
 
     /**
@@ -27,6 +29,7 @@ public class FileInfoServiceImpl implements FileInfoService {
         try {
             return fileInfoRepo.findAllInDirectory(dirId);
         } catch (Exception e) {
+            log.error(String.format("Failed to retrieve all file in directory with id %d from the database.", dirId));
             throw new DatabaseException("Couldn't find all files in directory!", e);
         }
     }
@@ -40,6 +43,7 @@ public class FileInfoServiceImpl implements FileInfoService {
         try {
             return fileInfoRepo.findById(fileId).orElseThrow();
         } catch (Exception e) {
+            log.error(String.format("Failed to retrieve file info for file with id %d from the database.", fileId));
             throw new DatabaseException("Couldn't find File Info!", e);
         }
     }
@@ -53,6 +57,10 @@ public class FileInfoServiceImpl implements FileInfoService {
         try {
             return fileInfoRepo.save(fileInfo);
         } catch (Exception e) {
+            log.error(String.format("Failed to save file '%s' of type '%s' with size '%d' bytes to database.",
+                    fileInfo.getName(),
+                    fileInfo.getType(),
+                    fileInfo.getSize()));
             throw new DatabaseException("File Info couldn't be saved!", e);
         }
     }
@@ -66,6 +74,7 @@ public class FileInfoServiceImpl implements FileInfoService {
         try {
             fileInfoRepo.deleteById(fileId);
         } catch (Exception e) {
+            log.error(String.format("Failed to delete file with id %d", fileId));
             throw new DatabaseException("File Info couldn't be deleted!", e);
         }
     }
