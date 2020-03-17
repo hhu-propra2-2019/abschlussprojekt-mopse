ccd43cb003e47745a30bc9af6dbaf234517039dd
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 12:18:35 +0100

    UPDATE: suppress warning as it's a wrapper class

--- src/main/java/mops/businesslogic/FileQueryForm.java
+++ src/main/java/mops/businesslogic/FileQueryForm.java
@@ -1,10 +1,12 @@
 package mops.businesslogic;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
+@SuppressFBWarnings("EI_EXPOSE_REP")
 public class FileQueryForm {
     /**
      * List of file names to search for.
