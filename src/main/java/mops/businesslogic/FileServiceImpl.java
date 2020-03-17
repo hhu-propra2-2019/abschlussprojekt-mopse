ba3dbc1a3561bd6bd2637df316598121b025f0b0
realemx <49307158+realemx@users.noreply.github.com>
2020-03-17 12:02:29 +0100

    ADD: logging
    Co-authored-by: Ali Hassoun <ali-hassoun.97@hotmail.de>

--- src/main/java/mops/businesslogic/FileServiceImpl.java
+++ src/main/java/mops/businesslogic/FileServiceImpl.java
@@ -2,6 +2,7 @@ package mops.businesslogic;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mops.businesslogic.exception.DeleteAccessPermissionException;
 import mops.businesslogic.exception.FileNotFoundException;
 import mops.businesslogic.exception.ReadAccessPermissionException;
@@ -23,6 +24,7 @@ import java.util.Set;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class FileServiceImpl implements FileService {
 
     /**
@@ -49,7 +51,10 @@ public class FileServiceImpl implements FileService {
         UserPermission userPermission = directoryService.getPermissionsOfUser(account, dirId);
 
         if (!userPermission.isWrite()) {
-            throw new WriteAccessPermissionException("No write permission");
+            log.error("User {} tried to save a file without write permission.",
+                    account.getName(),
+                    new WriteAccessPermissionException("No write permission")
+            );
         }
         //no Law of demeter violation
         FileInfo meta = FileInfo.builder()
@@ -64,7 +69,12 @@ public class FileServiceImpl implements FileService {
             fileRepository.saveFile(multipartFile, fileInfo.getId());
         } catch (MopsException e) {
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
-            throw new MopsException("Error while saving", e);
+            log.error("Error while saving file {} by user {}. Error: {}",
+                    meta.getName(),
+                    account.getName(),
+                    e.getMessage(),
+                    new MopsException("Error while saving", e)
+            );
         }
     }
 
@@ -76,27 +86,41 @@ public class FileServiceImpl implements FileService {
     @SuppressFBWarnings(value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
             justification = "There's no nullcheck here.")
     public FileContainer getFile(Account account, long fileId) throws MopsException {
-
         FileInfo fileInfo;
         try {
             fileInfo = fileInfoService.fetchFileInfo(fileId);
         } catch (MopsException e) {
-            throw new FileNotFoundException("File not found", e);
+            log.error("User {} tried to retrieved non existing file with ID {}. Error: {}",
+                    account.getName(),
+                    fileId,
+                    e.getMessage()
+            );
+            // has to be extra thrown here, else there will be an initialization error
+            throw new FileNotFoundException("File with ID " + fileId + " not found", e);
         }
 
         UserPermission userPermission = directoryService.getPermissionsOfUser(account, fileInfo.getDirectoryId());
 
         if (!userPermission.isRead()) {
-            throw new ReadAccessPermissionException("No read permission");
+            log.error("User {} tried to read file {} without permission.",
+                    account.getName(),
+                    fileId,
+                    new ReadAccessPermissionException("No read permission")
+            );
         }
 
         try (InputStream stream = fileRepository.getFileContent(fileId)) {
             ByteArrayResource byteArrayResource = new ByteArrayResource(stream.readAllBytes());
             return new FileContainer(fileInfo, byteArrayResource);
         } catch (MopsException | IOException e) {
+            log.error("Error on retrieving file with ID {}. Error: {}",
+                    fileId,
+                    e.getMessage()
+            );
             throw new MopsException("Error while retrieving", e);
         }
     }
+
     /**
      * {@inheritDoc}
      */
@@ -108,6 +132,10 @@ public class FileServiceImpl implements FileService {
         try {
             fileInfo = fileInfoService.fetchFileInfo(fileId);
         } catch (MopsException e) {
+            log.error("User {} tried to delete file with ID {}, bot file was not found.",
+                    account.getName(),
+                    fileId
+            );
             throw new FileNotFoundException("File not found", e);
         }
 
@@ -116,6 +144,10 @@ public class FileServiceImpl implements FileService {
         boolean isOwner = owner.equals(account.getName());
         // Only true if user is not the owner and has no delete permission
         if (!isOwner && !userPermission.isDelete()) {
+            log.error("User {} tried to delete file with ID {} without permission.",
+                    account.getName(),
+                    fileId
+            );
             throw new DeleteAccessPermissionException("No delete permission");
         }
 
@@ -124,6 +156,10 @@ public class FileServiceImpl implements FileService {
             fileRepository.deleteFile(fileId);
         } catch (MopsException e) {
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            log.error("File with ID {} error on retrieving. Error: {}",
+                    fileId,
+                    e.getMessage()
+            );
             throw new MopsException("Error while deleting", e);
         }
         return directoryService.getDirectory(fileInfo.getDirectoryId());
@@ -139,12 +175,21 @@ public class FileServiceImpl implements FileService {
         try {
             fileInfo = fileInfoService.fetchFileInfo(fileId);
         } catch (MopsException e) {
+            log.error("User {} tried to retrieved non existing file with ID {}. Error: {}",
+                    account.getName(),
+                    fileId,
+                    e.getMessage()
+            );
             throw new FileNotFoundException("File not found", e);
         }
         UserPermission userPermission = directoryService.getPermissionsOfUser(account, fileInfo.getDirectoryId());
 
         if (!userPermission.isRead()) {
-            throw new ReadAccessPermissionException("No read permission");
+            log.error("User {} tried to read file {} without permission.",
+                    account.getName(),
+                    fileId,
+                    new ReadAccessPermissionException("No read permission")
+            );
         }
         return fileInfoService.fetchFileInfo(fileId);
     }
@@ -157,7 +202,11 @@ public class FileServiceImpl implements FileService {
     public List<FileInfo> getFilesOfDirectory(Account account, long dirId) throws MopsException {
         UserPermission userPermission = directoryService.getPermissionsOfUser(account, dirId);
         if (!userPermission.isRead()) {
-            throw new ReadAccessPermissionException("No read permission");
+            log.error("User {} tried to read files in directory with ID {} without permission.",
+                    account.getName(),
+                    dirId,
+                    new ReadAccessPermissionException("No read permission")
+            );
         }
         return fileInfoService.fetchAllFilesInDirectory(dirId);
     }
