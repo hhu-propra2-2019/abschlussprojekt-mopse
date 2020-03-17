c437b66fbc4c84b935b2541f9ed894c920478a0e
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-17 11:36:19 +0100

    ADD: JavaDoc annotations

--- src/main/java/mops/businesslogic/FileQueryForm.java
+++ src/main/java/mops/businesslogic/FileQueryForm.java
@@ -6,11 +6,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class FileQueryForm {
+    /**
+     * List of file names to search for.
+     */
     private String[] fileNames;
-
+    /**
+     * List of user names of file owners to search for.
+     */
     private String[] owners;
-
+    /**
+     * List of file types to search for.
+     */
     private String[] types;
 
+    /**
+     * List of file tags to search for.
+     */
     private String[] tags;
 }
