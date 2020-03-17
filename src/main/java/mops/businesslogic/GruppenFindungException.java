480983458907824cb0794054e54bacde9575c7d3
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 11:58:52 +0100

    ADD: external api exception

---
+++ src/main/java/mops/businesslogic/GruppenFindungException.java
@@ -0,0 +1,9 @@
+package mops.businesslogic;
+
+import mops.exception.MopsException;
+
+public class GruppenFindungException extends MopsException {
+    public GruppenFindungException(String message) {
+        super(message);
+    }
+}
