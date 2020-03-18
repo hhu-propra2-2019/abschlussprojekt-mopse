27999180cc0ebdacd7701c99573eab84a54c84d2
Segelzwerg <marcel.haas@hhu.de>
2020-03-18 12:23:30 +0100

    ADD: test for fetch all groups for user

--- src/test/java/mops/businesslogic/PermissionServiceTest.java
+++ src/test/java/mops/businesslogic/PermissionServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Set;
 
 import static mops.businesslogic.PermissionServiceProdImpl.URL;
@@ -25,6 +26,7 @@ class PermissionServiceTest {
     RestTemplate restTemplate;
     long groupId;
     String userName;
+    private Account carlo;
 
     @BeforeEach
     void setup() {
@@ -32,6 +34,8 @@ class PermissionServiceTest {
         restTemplate = mock(RestTemplate.class);
         permissionService = new PermissionServiceProdImpl(restTemplate);
         userName = "Carlo";
+        carlo = Account.of(userName, "carlo@hhu.de", "admin");
+
     }
 
     @Test
@@ -39,7 +43,6 @@ class PermissionServiceTest {
         Set<PermissionServiceProdImpl.GroupPermission> groups =
                 Set.of(new PermissionServiceProdImpl.GroupPermission(groupId, "admin"));
         PermissionServiceProdImpl.Permission permission = new PermissionServiceProdImpl.Permission(userName, groups);
-        Account carlo = Account.of(userName, "carlo@hhu.de", "admin");
 
         when(restTemplate.getForObject(URL, PermissionServiceProdImpl.Permission.class)).thenReturn(permission);
 
@@ -61,7 +64,6 @@ class PermissionServiceTest {
 
     @Test
     void fetchRoleExceptionThrownTest() {
-        Account carlo = Account.of(userName, "carlo@hhu.de", "admin");
         when(restTemplate.getForObject(URL, PermissionServiceProdImpl.Permission.class)).thenReturn(null);
 
         assertThatExceptionOfType(GruppenFindungException.class).isThrownBy(() -> permissionService.fetchRoleForUserInGroup(carlo, groupId));
@@ -71,5 +73,19 @@ class PermissionServiceTest {
     void fetchRolesExceptionThrownTest() {
         when(restTemplate.getForObject(URL, PermissionServiceProdImpl.GroupPermission[].class)).thenReturn(null);
         assertThatExceptionOfType(GruppenFindungException.class).isThrownBy(() -> permissionService.fetchRolesInGroup(groupId));
+    }
+
+    @Test
+    public void fetchAllGroupsForUserTest() {
+        long[] groups = { 1L, 2L };
+        List<Group> expectedGroups = List.of(
+                new Group(1L, "test1", 1L),
+                new Group(2L, "test2", 2L)
+        );
+        when(restTemplate.getForObject(URL, long[].class)).thenReturn(groups);
+
+        List<Group> requestedGroups = permissionService.fetchGroupsForUser(carlo);
+
+        assertThat(requestedGroups).isEqualTo(expectedGroups);
     }
 }
