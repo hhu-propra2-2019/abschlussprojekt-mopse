3bb0d708331908f80b2de5b3a3d6baadaa1a9d8a
realemx <49307158+realemx@users.noreply.github.com>
2020-03-18 10:16:00 +0100

    ADD: Keycloak restTemplate bean

--- src/main/java/mops/config/KeycloakConfig.java
+++ src/main/java/mops/config/KeycloakConfig.java
@@ -1,8 +1,14 @@
 package mops.config;
 
+import org.keycloak.OAuth2Constants;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+
 
 /**
  * WORKAROUND for https://issues.redhat.com/browse/KEYCLOAK-11282.
@@ -10,7 +16,25 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
+@SuppressWarnings("PMD")
 public class KeycloakConfig {
+    /**
+     * Keycloak ressource.
+     */
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    /**
+     * Keycloak credentials.
+     */
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    /**
+     * Keycloak token URI.
+     */
+    @Value("${hhu_keycloak.token-uri}")
+    private String tokenUri;
 
     /**
      * Necessary keycloak config.
@@ -20,6 +44,21 @@ public class KeycloakConfig {
     @Bean
     public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    /**
+     * Creates a keycloak secured keycloak restTemplate.
+     * @return secured restTemplate
+     */
+    @Bean
+    public RestTemplate serviceAccountRestTemplate() {
+        ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
+        resourceDetails.setGrantType(OAuth2Constants.CLIENT_CREDENTIALS);
+        resourceDetails.setAccessTokenUri(tokenUri);
+        resourceDetails.setClientId(clientId);
+        resourceDetails.setClientSecret(clientSecret);
+
+        return new OAuth2RestTemplate(resourceDetails);
     }
 
 }
