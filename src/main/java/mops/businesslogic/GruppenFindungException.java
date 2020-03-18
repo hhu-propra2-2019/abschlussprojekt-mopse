d3acf51853be417a586c231d02a5c3ac6ec4e84e
Segelzwerg <marcel.haas@hhu.de>
2020-03-18 09:47:00 +0100

    ADD: java doc

--- src/main/java/mops/businesslogic/GruppenFindungException.java
+++ src/main/java/mops/businesslogic/GruppenFindungException.java
@@ -3,6 +3,11 @@ package mops.businesslogic;
 import mops.exception.MopsException;
 
 public class GruppenFindungException extends MopsException {
+    /**
+     * Is thrown when something happened during API request to GruppenFindung.
+     *
+     * @param message what happened
+     */
     public GruppenFindungException(String message) {
         super(message);
     }
