36582c1f247a29492155905035d2a091cc844c4d
Segelzwerg <marcel.haas@hhu.de>
2020-03-19 10:09:28 +0100

    UPDATE: use env variable for url

--- src/main/java/mops/businesslogic/GroupServiceProdImpl.java
+++ src/main/java/mops/businesslogic/GroupServiceProdImpl.java
@@ -3,6 +3,7 @@ package mops.businesslogic;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mops.exception.MopsException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +17,10 @@ import java.util.List;
 @Profile("prod")
 public class GroupServiceProdImpl implements GroupService {
     /**
-     * URL to GruppenFindung. Will be replaced with env variable.
+     * URL to GruppenFindung.
      */
-    public static final String URL = "https://mops.hhu.de/gruppe1/";
+    @Value("${material1.mops.gruppenfindung.url}")
+    public static String gruppenFindungUrl;
 
     /**
      * Directory Service.
@@ -35,7 +37,7 @@ public class GroupServiceProdImpl implements GroupService {
      */
     @Override
     public List<Group> getAllGroups(Account account) throws MopsException {
-        Group[] groups = restTemplate.getForObject(URL, Group[].class);
+        Group[] groups = restTemplate.getForObject(gruppenFindungUrl, Group[].class);
         if (groups == null) {
             log.error("The request for groups of user {} failed.", account.getName());
             throw new GruppenFindungException(String.format(
