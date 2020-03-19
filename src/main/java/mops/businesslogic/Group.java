90de8b939abdb9622552e9bc3247c7ed4775a865
Segelzwerg <marcel.haas@hhu.de>
2020-03-19 09:47:42 +0100

    UPDATE: suppress warnings

--- src/main/java/mops/businesslogic/Group.java
+++ src/main/java/mops/businesslogic/Group.java
@@ -6,6 +6,7 @@ import lombok.Value;
  * Represents a group (course, study group, etc.).
  */
 @Value
+@SuppressWarnings({ "checkstyle:VisibilityModifier", "PMD.DefaultPackage" })
 public class Group {
 
     /**
