b2fe80cb6c8e2b1de9ee7d4aed39f3db6b1ea63c
iTitus <iTitus@users.noreply.github.com>
2020-03-19 18:58:59 +0100

    ADD: log exceptions
    Co-authored-by: Marcel <marcel.haas@hhu.de>

--- src/main/java/mops/businesslogic/prometheus/PrometheusComponent.java
+++ src/main/java/mops/businesslogic/prometheus/PrometheusComponent.java
@@ -59,7 +59,8 @@ public class PrometheusComponent {
                 .builder("mops.material1.totalStorageUsage", () -> {
                             try {
                                 return fileInfoService.getTotalStorageUsage();
-                            } catch (MopsException ignored) {
+                            } catch (MopsException e) {
+                                log.error("Error while reading total file storage usage from gauge:", e);
                                 return 0L;
                             }
                         }
@@ -69,11 +70,12 @@ public class PrometheusComponent {
         log.debug("Adding total file count gauge.");
         Gauge
                 .builder("mops.material1.totalFileCount", () -> {
-                            try {
-                                return fileInfoService.getTotalFileCount();
-                            } catch (MopsException ignored) {
-                                return 0L;
-                            }
+                    try {
+                        return fileInfoService.getTotalFileCount();
+                    } catch (MopsException e) {
+                        log.error("Error while reading total file count from gauge:", e);
+                        return 0L;
+                    }
                         }
                 )
                 .register(meterRegistry);
@@ -81,11 +83,12 @@ public class PrometheusComponent {
         log.debug("Adding total directory count gauge.");
         Gauge
                 .builder("mops.material1.totalDirCount", () -> {
-                            try {
-                                return directoryService.getTotalDirCount();
-                            } catch (MopsException ignored) {
-                                return 0L;
-                            }
+                    try {
+                        return directoryService.getTotalDirCount();
+                    } catch (MopsException e) {
+                        log.error("Error while reading total file directory count from gauge:", e);
+                        return 0L;
+                    }
                         }
                 )
                 .register(meterRegistry);
@@ -96,18 +99,23 @@ public class PrometheusComponent {
         List<Group> groups = List.of();
         try {
             groups = groupService.getAllGroups();
-        } catch (MopsException ignored) {
+        } catch (MopsException e) {
+            log.error("Error while getting all groups from gauge setup:", e);
         }
 
         groups.forEach(group -> {
             log.debug("Adding group storage gauge for group {}.", group);
             Gauge
                     .builder("mops.material1.groupStorageUsage", () -> {
-                                try {
-                                    return fileInfoService.getStorageUsageInGroup(group.getId());
-                                } catch (MopsException ignored) {
-                                    return 0L;
-                                }
+                        try {
+                            return fileInfoService.getStorageUsageInGroup(group.getId());
+                        } catch (MopsException e) {
+                            log.error(
+                                    "Error while reading file storage usage in group '{}' from gauge:",
+                                    group, e
+                            );
+                            return 0L;
+                        }
                             }
                     )
                     .tag("group_id", String.valueOf(group.getId()))
@@ -118,11 +126,15 @@ public class PrometheusComponent {
             log.debug("Adding group file count gauge for group {}.", group);
             Gauge
                     .builder("mops.material1.groupFileCount", () -> {
-                                try {
-                                    return fileInfoService.getFileCountInGroup(group.getId());
-                                } catch (MopsException ignored) {
-                                    return 0L;
-                                }
+                        try {
+                            return fileInfoService.getFileCountInGroup(group.getId());
+                        } catch (MopsException e) {
+                            log.error(
+                                    "Error while reading file count in group '{}' from gauge:",
+                                    group, e
+                            );
+                            return 0L;
+                        }
                             }
                     )
                     .tag("group_id", String.valueOf(group.getId()))
@@ -133,11 +145,15 @@ public class PrometheusComponent {
             log.debug("Adding group dir count gauge for group {}.", group);
             Gauge
                     .builder("mops.material1.groupDirCount", () -> {
-                                try {
-                                    return directoryService.getDirCountInGroup(group.getId());
-                                } catch (MopsException ignored) {
-                                    return 0L;
-                                }
+                        try {
+                            return directoryService.getDirCountInGroup(group.getId());
+                        } catch (MopsException e) {
+                            log.error(
+                                    "Error while reading directory count in group '{}' from gauge:",
+                                    group, e
+                            );
+                            return 0L;
+                        }
                             }
                     )
                     .tag("group_id", String.valueOf(group.getId()))
