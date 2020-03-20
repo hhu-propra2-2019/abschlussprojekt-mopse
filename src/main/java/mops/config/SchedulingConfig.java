8ac47cf1066c0d1848798f114a04d0bbe69b0b13
iTitus <iTitus@users.noreply.github.com>
2020-03-20 11:19:50 +0100

    FIX: add new gauges when there are new groups. (#222)
    * FIX: add new gauges when there are new groups.
    
    Co-Authored-By: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * FIX: sql queries
    
    Co-Authored-By: Kai <49519629+KaFe23@users.noreply.github.com>
    Co-authored-by: Pascal Liedtke <49307158+realemx@users.noreply.github.com>
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    Co-authored-by: Pascal Liedtke <49307158+realemx@users.noreply.github.com>

---
+++ src/main/java/mops/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package mops.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
