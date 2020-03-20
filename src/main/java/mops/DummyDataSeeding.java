f0ce6f6af5b8e1753355628026640605d274feae
Segelzwerg <marcel.haas@hhu.de>
2020-03-20 10:37:59 +0100

    FIX: correct role for orga

--- src/main/java/mops/DummyDataSeeding.java
+++ src/main/java/mops/DummyDataSeeding.java
@@ -33,7 +33,7 @@ public class DummyDataSeeding {
 
             DirectoryPermissions directoryPermissions = DirectoryPermissions.builder()
                     .entry("admin", true, true, true)
-                    .entry("orga", true, true, true)
+                    .entry("editor", true, true, true)
                     .entry("korrektor", true, true, true)
                     .entry("viewer", true, true, true)
                     .entry("studentin", true, true, true)
