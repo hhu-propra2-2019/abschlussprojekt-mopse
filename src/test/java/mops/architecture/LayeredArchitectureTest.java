0b170ed54a3be4afe33b15ecaa78b719dc28bfef
iTitus <iTitus@users.noreply.github.com>
2020-03-20 15:07:52 +0100

    HOTFIX: dummy data initialization (#232)
    * FIX: dummy data initialization
    
    * FIX: pmd

--- src/test/java/mops/architecture/LayeredArchitectureTest.java
+++ src/test/java/mops/architecture/LayeredArchitectureTest.java
@@ -4,16 +4,6 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import mops.DummyDataSeeding;
-import mops.persistence.DirectoryPermissionsRepository;
-import mops.persistence.DirectoryRepository;
-import mops.persistence.FileInfoRepository;
-import mops.persistence.directory.Directory;
-import mops.persistence.directory.DirectoryBuilder;
-import mops.persistence.file.FileInfo;
-import mops.persistence.file.FileInfoBuilder;
-import mops.persistence.permission.DirectoryPermissions;
-import mops.persistence.permission.DirectoryPermissionsBuilder;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 import static mops.architecture.ArchitectureRuleConfig.*;
@@ -27,23 +17,14 @@ class LayeredArchitectureTest {
      */
     @ArchTest
     static final ArchRule checkLayeredArchitecture = layeredArchitecture()
+            .layer("mops").definedBy(MOPS)
             .layer("mopsPersistence").definedBy(MOPS_PERSISTENCE)
             .layer("mopsBusinesslogic").definedBy(MOPS_BUSINESSLOGIC)
             .layer("mopsPresentation").definedBy(MOPS_PRESENTATION)
 
-            .whereLayer("mopsPresentation").mayNotBeAccessedByAnyLayer()
-            .whereLayer("mopsBusinesslogic").mayOnlyBeAccessedByLayers("mopsPresentation")
-            .whereLayer("mopsPersistence").mayOnlyBeAccessedByLayers("mopsPresentation", "mopsBusinesslogic")
-
-            // This is needed so that we can use DummyData to test the application.
-            .ignoreDependency(DummyDataSeeding.class, Directory.class)
-            .ignoreDependency(DummyDataSeeding.class, DirectoryRepository.class)
-            .ignoreDependency(DummyDataSeeding.class, DirectoryPermissions.class)
-            .ignoreDependency(DummyDataSeeding.class, DirectoryPermissionsRepository.class)
-            .ignoreDependency(DummyDataSeeding.class, DirectoryPermissionsBuilder.class)
-            .ignoreDependency(DummyDataSeeding.class, DirectoryBuilder.class)
-            .ignoreDependency(DummyDataSeeding.class, FileInfo.class)
-            .ignoreDependency(DummyDataSeeding.class, FileInfoRepository.class)
-            .ignoreDependency(DummyDataSeeding.class, FileInfoBuilder.class);
+            .whereLayer("mops").mayNotBeAccessedByAnyLayer()
+            .whereLayer("mopsPresentation").mayOnlyBeAccessedByLayers("mops")
+            .whereLayer("mopsBusinesslogic").mayOnlyBeAccessedByLayers("mops", "mopsPresentation")
+            .whereLayer("mopsPersistence").mayOnlyBeAccessedByLayers("mops", "mopsPresentation", "mopsBusinesslogic");
 
 }
