c91482bba73c813ca60a11fc87b26e170eb1bcb9
realemx <49307158+realemx@users.noreply.github.com>
2020-03-24 11:32:30 +0100

    ADD: error page handling

--- src/main/java/mops/presentation/ExceptionController.java
+++ src/main/java/mops/presentation/ExceptionController.java
@@ -1,9 +1,14 @@
 package mops.presentation;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,15 +16,9 @@ import javax.servlet.http.HttpServletResponse;
  * Exception Handling.
  */
 @Controller
-public class ExceptionController implements HandlerExceptionResolver {
+public class ExceptionController implements HandlerExceptionResolver, ErrorController {
     /**
-     * Exception handling.
-     *
-     * @param request  request
-     * @param response response
-     * @param handler  handler
-     * @param ex       Exception
-     * @return Error view
+     * {@inheritDoc}
      */
     @Override
     @SuppressWarnings("PMD.LawOfDemeter")
@@ -32,5 +31,34 @@ public class ExceptionController implements HandlerExceptionResolver {
         mav.getModel().put("error", ex);
         mav.getModel().put("referrer", referrer);
         return mav;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String referrer = request.getHeader("referer");
+        model.addAttribute("referrer", referrer);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "mops_error";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getErrorPath() {
+        return "mops_error";
     }
 }
