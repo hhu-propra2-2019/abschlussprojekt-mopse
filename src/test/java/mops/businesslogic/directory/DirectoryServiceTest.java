d88a14d47124862c85e15794e32f513ec29074d0
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-24 11:56:47 +0100

    UPDATE: searchFolder test again

--- src/test/java/mops/businesslogic/directory/DirectoryServiceTest.java
+++ src/test/java/mops/businesslogic/directory/DirectoryServiceTest.java
@@ -6,6 +6,7 @@ import mops.businesslogic.group.GroupService;
 import mops.businesslogic.security.Account;
 import mops.exception.MopsException;
 import mops.persistence.DirectoryPermissionsRepository;
+import mops.persistence.DirectoryRepository;
 import mops.persistence.FileInfoRepository;
 import mops.persistence.FileRepository;
 import mops.persistence.directory.Directory;
@@ -193,10 +194,12 @@ class DirectoryServiceTest {
                         .build()
         );
 
+        Directory child = directoryService.createFolder(admin, root.getId(), "child");
+
         FileInfo matchingFile2 = fileInfoRepository.save(
                 FileInfo.builder()
                         .name("ab")
-                        .directory(root)
+                        .directory(child)
                         .type("txt")
                         .size(0L)
                         .owner(USER)
