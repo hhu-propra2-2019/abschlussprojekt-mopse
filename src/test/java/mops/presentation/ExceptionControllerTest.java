0b19c9745912cd9e2c91798fda3494fc50b5966c
realemx <49307158+realemx@users.noreply.github.com>
2020-03-24 12:46:42 +0100

    ADD: Test

--- src/test/java/mops/presentation/ExceptionControllerTest.java
+++ src/test/java/mops/presentation/ExceptionControllerTest.java
@@ -9,27 +9,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @KeycloakContext
-@WebMvcTest(Material1Controller.class)
-class Material1ControllerTest extends ServletKeycloakAuthUnitTestingSupport {
-
+@WebMvcTest(ExceptionController.class)
+public class ExceptionControllerTest extends ServletKeycloakAuthUnitTestingSupport {
     @Test
     @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
-    void index() throws Exception {
-        mockMvc().perform(get("/material1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/material1/groups"))
-                .andDo(document("index/Material1Controller/{method-name}"));
-    }
-
-    /**
-     * Test if route is secured.
-     */
-    @Test
-    void notSignedIn() throws Exception {
-        mockMvc().perform(get("/material1"))
-                .andExpect(status().is3xxRedirection());
+    void error() throws Exception {
+        mockMvc().perform(get("/material1/error"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("mops_error"))
+                .andDo(document("index/ExceptionController/{method-name}"));
     }
 }
