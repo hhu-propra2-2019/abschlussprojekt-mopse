498a25a8e38947be5351dee80d9846a62b311c03
iTitus <iTitus@users.noreply.github.com>
2020-03-24 14:10:42 +0100

    FIX: add account to model on error

--- src/main/java/mops/presentation/ExceptionController.java
+++ src/main/java/mops/presentation/ExceptionController.java
@@ -1,12 +1,18 @@
 package mops.presentation;
 
+import lombok.extern.slf4j.Slf4j;
+import mops.businesslogic.security.Account;
+import mops.presentation.error.ExceptionPresentationError;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.FlashMap;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -15,8 +21,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Exception Handling.
  */
+@Slf4j
 @Controller
 public class ExceptionController implements HandlerExceptionResolver, ErrorController {
+
     /**
      * {@inheritDoc}
      */
@@ -24,21 +32,27 @@ public class ExceptionController implements HandlerExceptionResolver, ErrorContr
     @SuppressWarnings("PMD.LawOfDemeter")
     public ModelAndView resolveException(HttpServletRequest request,
                                          HttpServletResponse response,
-                                         Object handler, Exception ex) {
-
-        String referer = request.getHeader("referer");
-        ModelAndView mav = new ModelAndView("mops_error");
-        mav.getModel().put("error", ex);
-        mav.getModel().put("referer", referer);
+                                         Object handler,
+                                         Exception ex) {
+        log.error("Caught exception from controller:", ex);
+        ModelAndView mav = new ModelAndView("redirect:/material1/error");
+        FlashMap flashMap = RequestContextUtils.getOutputFlashMap(request);
+        flashMap.put("error", new ExceptionPresentationError(ex));
         return mav;
     }
 
     /**
-     * {@inheritDoc}
+     * Error mapping.
+     *
+     * @param token   auth token
+     * @param request http request
+     * @param model   model
+     * @return view
      */
     @RequestMapping("/material1/error")
     @SuppressWarnings({ "PMD.LawOfDemeter", "PMD.DataflowAnomalyAnalysis" })
-    public String handleError(HttpServletRequest request, Model model) {
+    public String handleError(KeycloakAuthenticationToken token, HttpServletRequest request, Model model) {
+        Account account = Account.of(token);
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         String referer = request.getHeader("referer");
         model.addAttribute("referer", referer);
@@ -60,6 +74,7 @@ public class ExceptionController implements HandlerExceptionResolver, ErrorContr
             }
         }
 
+        model.addAttribute("account", account);
         return "mops_error";
     }
 
