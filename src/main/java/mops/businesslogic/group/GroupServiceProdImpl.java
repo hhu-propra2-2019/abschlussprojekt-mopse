1c58eb5ed64c0a7dd8940c44331a1ecc291cdf06
iTitus <iTitus@users.noreply.github.com>
2020-03-25 17:14:19 +0100

    FEATURE: demo docker compose (#292)
    * ADD: demo docker compose
    
    * ADD: license
    
    * ADD: documentation
    
    * FIX: documentation
    
    * REMOVE: warning
    
    * FIX: readme
    
    * FIX: typo
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * FIX: typos
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * FIX: typos
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * REMOVE: whitespaces
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * FIX: pmd
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    * FIX: spotbugs
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>
    
    Co-authored-by: Kai <49519629+KaFe23@users.noreply.github.com>

--- src/main/java/mops/businesslogic/group/GroupServiceProdImpl.java
+++ src/main/java/mops/businesslogic/group/GroupServiceProdImpl.java
@@ -1,5 +1,6 @@
 package mops.businesslogic.group;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +13,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -43,67 +41,77 @@ public class GroupServiceProdImpl implements GroupService {
      * {@inheritDoc}
      */
     @Override
-    @SuppressWarnings("PMD.LawOfDemeter")
+    @SuppressWarnings({ "PMD.LawOfDemeter", "PMD.AvoidCatchingGenericException" })
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "spotbugs bug")
     public String fetchRoleForUserInGroup(Account account, long groupId) throws MopsException {
-        log.info("Request role for user {} in group {}", account.getName(), groupId);
-        Permission permission = restTemplate.getForObject(gruppenFindungUrl + "/get-permission", Permission.class);
-        if (permission == null) {
+        try {
+            Permission permission = restTemplate.getForObject(gruppenFindungUrl + "/get-permission", Permission.class);
+            return Objects.requireNonNull(permission).getRoleInGroup(groupId);
+        } catch (Exception e) {
             log.error("The request for user roles for user '{}' in group {} failed.", account.getName(), groupId);
             throw new GruppenFindungException(String.format(
                     "Es konnte keine Rolle für Sie in Gruppe %d gefunden werden.",
-                    groupId));
+                    groupId), e);
         }
-        return permission.getRoleInGroup(groupId);
+
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    @SuppressWarnings("PMD.LawOfDemeter") // stream
+    @SuppressWarnings({ "PMD.LawOfDemeter", "PMD.AvoidCatchingGenericException" }) // stream
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "spotbugs bug")
     public Set<String> fetchRolesInGroup(long groupId) throws MopsException {
-        log.info("Request roles for group {}", groupId);
-        GroupPermission[] groupPermissions = restTemplate.getForObject(gruppenFindungUrl + "/get-roles",
-                GroupPermission[].class);
-        if (groupPermissions == null) {
+        try {
+            GroupPermission[] groupPermissions = restTemplate.getForObject(gruppenFindungUrl + "/get-roles",
+                    GroupPermission[].class);
+            return Arrays.stream(Objects.requireNonNull(groupPermissions))
+                    .map(GroupPermission::getPermission)
+                    .collect(Collectors.toSet());
+        } catch (Exception e) {
             log.error("The request for roles in group {} failed.", groupId);
             throw new GruppenFindungException(String.format(
                     "Es konnten keinen Rollen für diese Gruppe %d gefunden werden.",
-                    groupId));
+                    groupId), e);
         }
-        return Arrays.stream(groupPermissions)
-                .map(GroupPermission::getPermission)
-                .collect(Collectors.toSet());
+
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "spotbugs bug")
     public List<Group> getAllGroups() throws MopsException {
         // TODO: change to real route once known
-        Group[] groups = restTemplate.getForObject(gruppenFindungUrl + "/get-all-groups", Group[].class);
-        if (groups == null) {
+        try {
+            Group[] groups = restTemplate.getForObject(gruppenFindungUrl + "/get-all-groups", Group[].class);
+            return List.of(Objects.requireNonNull(groups));
+        } catch (Exception e) {
             log.error("The request for all groups failed.");
-            throw new GruppenFindungException("Es konnten keinen Gruppen gefunden werden.");
+            throw new GruppenFindungException("Es konnten keinen Gruppen gefunden werden.", e);
         }
-        return List.of(groups);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "spotbugs bug")
     public List<Group> getAllGroupsOfUser(Account account) throws MopsException {
         // TODO: change to real route once known
-        Group[] groups = restTemplate.getForObject(gruppenFindungUrl + "/get-all-groups-from-user", Group[].class);
-        if (groups == null) {
+        try {
+            Group[] groups = restTemplate.getForObject(gruppenFindungUrl + "/get-all-groups-from-user", Group[].class);
+            return List.of(Objects.requireNonNull(groups));
+        } catch (Exception e) {
             log.error("The request for groups of user {} failed.", account.getName());
             throw new GruppenFindungException(String.format(
                     "Es konnten keinen Gruppen für die Nutzerin '%s' gefunden werden.",
-                    account.getName()));
+                    account.getName()), e);
         }
-        return List.of(groups);
     }
 
     /**
