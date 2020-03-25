0d4881ba25e0203b088f02a3e3e04f6cce118dbe
Marcel <marcel.haas@hhu.de>
2020-03-25 12:40:11 +0100

    Feature/duplication error (#287)
    * ADD: database duplication error
    
    * UPDATE: throw duplication error
    
    * FIX: trailing space
    
    * FIX: preserver stacktrace
    
    * ADD: param java doc
    
    * ADD: test for duplication save
    
    * FIX: miles line
    
    Co-Authored-By: iTitus <iTitus@users.noreply.github.com>
    
    Co-authored-by: iTitus <iTitus@users.noreply.github.com>

---
+++ src/main/java/mops/businesslogic/exception/DatabaseDuplicationException.java
@@ -0,0 +1,25 @@
+package mops.businesslogic.exception;
+
+/**
+ * Is thrown if an entry already exists.
+ */
+public class DatabaseDuplicationException extends DatabaseException {
+    /**
+     * Is thrown if the an entry already exists.
+     *
+     * @param message error message
+     */
+    public DatabaseDuplicationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Is thrown if the an entry already exists.
+     *
+     * @param message error message
+     * @param exception the wrapped exception
+     */
+    public DatabaseDuplicationException(String message, Exception exception) {
+        super(message, exception);
+    }
+}
