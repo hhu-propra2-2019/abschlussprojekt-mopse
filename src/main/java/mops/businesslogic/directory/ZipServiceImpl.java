60e6e20095c2e95ea83c45d3900ced5f20d21c15
Segelzwerg <marcel.haas@hhu.de>
2020-03-26 13:57:51 +0100

    ADD: zip impl skeleton

---
+++ src/main/java/mops/businesslogic/directory/ZipServiceImpl.java
@@ -0,0 +1,17 @@
+package mops.businesslogic.directory;
+
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Zips content from File Storage.
+ */
+public class ZipServiceImpl implements ZipService {
+    /**
+    * {@inheritDoc}
+     */
+    @Override
+    public ZipOutputStream zipDirectory(long dirId) {
+
+        return null;
+    }
+}
