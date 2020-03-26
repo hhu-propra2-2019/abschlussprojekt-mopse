2f58371d7d62e668147bf8216e035da84be6d452
Segelzwerg <marcel.haas@hhu.de>
2020-03-26 12:37:22 +0100

    ADD: test for group add verification

--- src/test/java/mops/businesslogic/gruppenfindung/GroupUpdaterTest.java
+++ src/test/java/mops/businesslogic/gruppenfindung/GroupUpdaterTest.java
@@ -3,7 +3,9 @@ package mops.businesslogic.gruppenfindung;
 import mops.businesslogic.event.LatestEventIdService;
 import mops.businesslogic.group.GroupService;
 import mops.exception.MopsException;
+import mops.persistence.GroupRepository;
 import mops.persistence.event.LatestEventId;
+import mops.persistence.group.Group;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,10 +13,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class GroupUpdaterTest {
@@ -27,19 +31,21 @@ class GroupUpdaterTest {
     LatestEventIdService latestEventIdService;
     @Mock
     GroupService groupService;
+    @Mock
+    GroupRepository groupRepository;
 
     GroupUpdater groupUpdater;
+    private LatestEventId latestEventId;
 
     @BeforeEach
-    void setup() {
+    void setup() throws MopsException {
         groupUpdater = new GroupUpdater(gruppenfindungsService, latestEventIdService, groupService);
+        latestEventId = LatestEventId.of();
+        given(latestEventIdService.getLatestEventId()).willReturn(latestEventId);
     }
 
     @Test
     void latestEventIdGetsUpdated() throws MopsException {
-        LatestEventId latestEventId = LatestEventId.of();
-        given(latestEventIdService.getLatestEventId()).willReturn(latestEventId);
-
         UpdatedGroupsDTO updatedGroups = new UpdatedGroupsDTO();
         updatedGroups.setEventId(20L);
         updatedGroups.setGroupDAOs(List.of());
@@ -48,5 +54,39 @@ class GroupUpdaterTest {
         groupUpdater.updateDatabase();
 
         assertThat(latestEventId.getEventId()).isEqualTo(20L);
+    }
+
+    @Test
+    public void addingGroupTest() throws MopsException {
+        UUID groupId = new UUID(0, 6);
+        String groupName = "test";
+        String userName = "Thabb";
+
+        UpdatedGroupsDTO updatedGroups = new UpdatedGroupsDTO();
+        updatedGroups.setEventId(20L);
+
+        UserDTO user = new UserDTO();
+        user.setUsername(userName);
+
+        GroupDTO groupDTO = new GroupDTO();
+        groupDTO.setGroupId(groupId);
+        groupDTO.setGroupName(groupName);
+        groupDTO.setStatus(StatusDTO.ACTIVE);
+
+        updatedGroups.setGroupDAOs(List.of(groupDTO));
+
+        Group expectedGroup = Group.builder()
+                .groupId(groupId)
+                .name(groupName)
+                .member(userName, "admin")
+                .build();
+
+        given(gruppenfindungsService.getUpdatedGroups(anyLong())).willReturn(updatedGroups);
+        given(gruppenfindungsService.getMembers(groupId)).willReturn(List.of(user));
+        given(gruppenfindungsService.isUserAdminInGroup(userName, groupId)).willReturn(true);
+
+        groupUpdater.updateDatabase();
+
+        verify(groupRepository).save(expectedGroup);
     }
 }
