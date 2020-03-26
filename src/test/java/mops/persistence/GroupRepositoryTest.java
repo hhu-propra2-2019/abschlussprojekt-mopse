6e3d46f3e3e3885e6b407eb385d49cdce5a7dd24
Segelzwerg <marcel.haas@hhu.de>
2020-03-26 11:11:28 +0100

    ADD: test for multiple groups

--- src/test/java/mops/persistence/GroupRepositoryTest.java
+++ src/test/java/mops/persistence/GroupRepositoryTest.java
@@ -32,4 +32,41 @@ class GroupRepositoryTest {
         assertThat(groups).containsExactly(group);
     }
 
+    @Test
+    public void findMultipleGroupsTest() {
+        Group group1 = Group.builder()
+                .id(new UUID(0, 123))
+                .member("Segelzwerg", "admin")
+                .name("Root")
+                .build();
+
+        Group group2 = Group.builder()
+                .id(new UUID(0, 124))
+                .member("Segelzwerg", "admin")
+                .name("FirstLevel")
+                .build();
+
+        Group group3 = Group.builder()
+                .id(new UUID(0, 125))
+                .member("Segelzwerg", "admin")
+                .name("2ndLeve")
+                .build();
+
+        Group group4 = Group.builder()
+                .id(new UUID(0, 126))
+                .member("iTitus", "admin")
+                .name("Wrong")
+                .build();
+
+        List<Group> groups= List.of(group1, group2, group3, group4);
+
+        groupRepository.saveAll(groups);
+
+        List<Group> groupsOfUser1 = groupRepository.findByUser("Segelzwerg");
+        List<Group> groupsOfUser2 = groupRepository.findByUser("iTitus");
+
+        assertThat(groupsOfUser1).containsExactly(group1, group2, group3);
+        assertThat(groupsOfUser2).containsExactly(group4);
+
+    }
 }
