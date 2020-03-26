0721a3665e359767573caa038adba8b94cdde2b8
iTitus <iTitus@users.noreply.github.com>
2020-03-26 11:00:33 +0100

    ADD: missing test todo

---
+++ src/test/java/mops/businesslogic/gruppenfindung/GroupUpdaterTest.java
@@ -0,0 +1,7 @@
+package mops.businesslogic.gruppenfindung;
+
+class GroupUpdaterTest {
+
+    // TODO: test if the DTO objects are converted to our data objects correctly (adding, deleting etc.)
+
+}
