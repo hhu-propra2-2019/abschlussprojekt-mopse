c05ee5625dec0e1fb4d6b8b9139fe277f646af1f
iTitus <iTitus@users.noreply.github.com>
2020-03-26 20:15:37 +0100

    ADD: logout

--- src/main/java/mops/presentation/Material1Controller.java
+++ src/main/java/mops/presentation/Material1Controller.java
@@ -6,7 +6,9 @@ import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Controller for basic routes.
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 // demeter violations in logging
-@RequestMapping("/material1")
 @SuppressWarnings("PMD.LawOfDemeter")
 public class Material1Controller {
 
@@ -25,12 +26,24 @@ public class Material1Controller {
      * @param model spring view model
      * @return error view template
      */
-    @GetMapping
+    @GetMapping("/material1")
     public String index(KeycloakAuthenticationToken token, Model model) {
         Account account = Account.of(token);
         log.info("Index page requested by user '{}'.", account.getName());
 
         model.addAttribute("account", account);
         return "redirect:/material1/groups";
+    }
+
+    /**
+     * Logout Route.
+     *
+     * @param request servlet request
+     * @return redirect
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
     }
 }
