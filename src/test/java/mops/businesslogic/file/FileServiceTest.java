fd3293e33ac4b274bd3d5051c95a00a9c6029f84
realemx <49307158+realemx@users.noreply.github.com>
2020-03-30 12:12:03 +0200

    REMOVE: unused import

--- src/test/java/mops/businesslogic/file/FileServiceTest.java
+++ src/test/java/mops/businesslogic/file/FileServiceTest.java
@@ -14,7 +14,6 @@ import mops.persistence.file.FileInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
